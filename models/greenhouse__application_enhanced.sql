db1703a60d5c15f741dbe9d3662dc2314ae9c464
Avinash Kunnath <avinash.kunnath@fivetran.com>
2026-08-28 13:11:08 -0700

    Resolve new test scenario failures

--- models/greenhouse__application_enhanced.sql
+++ models/greenhouse__application_enhanced.sql
@@ -10,12 +10,14 @@ interview_metrics as (
     select
         source_relation,
         application_id,
-        {% if var('greenhouse_using_job_hiring_manager', True) %}
+        {% if var('greenhouse_using_job_hiring_manager', True) and var('greenhouse_using_interviewer', True) %}
         max(case when interviewer_is_hiring_manager then 1 else 0 end) as has_interviewed_w_hiring_manager,
         {% endif %}
         count(distinct scheduled_interview_id) as count_interviews,
+        {% if var('greenhouse_using_interviewer', True) %}
         count(distinct scorecard_id) as count_interview_scorecards,
         count(distinct interviewer_user_id) as count_distinct_interviewers,
+        {% endif %}
         max(starts_at) as latest_interview_scheduled_at
 
     from {{ ref('greenhouse__interview_enhanced') }}
@@ -30,12 +32,14 @@ final as (
         application.*
         {% if var('greenhouse_using_interview', True) %}
         ,
-        {% if var('greenhouse_using_job_hiring_manager', True) %}
+        {% if var('greenhouse_using_job_hiring_manager', True) and var('greenhouse_using_interviewer', True) %}
         coalesce(interview_metrics.has_interviewed_w_hiring_manager, 0) = 1 as has_interviewed_w_hiring_manager,
         {% endif %}
         coalesce(interview_metrics.count_interviews, 0) as count_interviews,
+        {% if var('greenhouse_using_interviewer', True) %}
         coalesce(interview_metrics.count_interview_scorecards, 0) as count_interview_scorecards,
         coalesce(interview_metrics.count_distinct_interviewers, 0) as count_distinct_interviewers,
+        {% endif %}
         interview_metrics.latest_interview_scheduled_at
         {% endif %}
 
